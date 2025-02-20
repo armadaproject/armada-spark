c1cd0c75ec4efbd5468fe89520347edd1592a44a
George Jahad <george@georgejahad.com>
2025-02-20 15:57:29 -0800

    fixed script
    Signed-off-by: George Jahad <george@georgejahad.com>

--- examples/submitSparkPi.sh
+++ examples/submitSparkPi.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+#command should be run like so:
+#  examples/submitSparkPi.sh local://armada://localhost:30002 ~/incoming/spark
+
+ARMADA_MASTER=$1
+SPARK_ROOT=$2
+ARMADA_SPARK_ROOT=`pwd`
 # run spark pi app on armada
-bin/spark-submit --master armada://localhost:30002 --deploy-mode cluster --name spark-pi --class org.apache.spark.examples.SparkPi --conf spark.executor.instances=2 --conf spark.kubernetes.container.image=spark:testing local:///opt/spark/examples/jars/spark-examples.jar 100
+SPARK_DRIVER_BIND_ADDRESS=localhost ~/incoming/spark/bin/spark-submit \
+  --master $ARMADA_MASTER \
+  --jars $ARMADA_SPARK_ROOT/target/armada-cluster-manager-1.0.0-SNAPSHOT.jar   --driver-class-path  $ARMADA_SPARK_ROOT/target/armada-cluster-manager-1.0.0-SNAPSHOT.jar  --name spark-pi --class org.apache.spark.examples.SparkPi --conf spark.executor.instances=2 --conf spark.kubernetes.container.image=spark:testing $SPARK_ROOT/examples/target/scala-2.13/jars/spark-examples_2.13-4.1.0-SNAPSHOT.jar 100
