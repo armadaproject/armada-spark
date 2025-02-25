dc183cafc13fc957beb466f6b92295f0b1094dd4
George Jahad <george@georgejahad.com>
2025-02-25 11:29:39 -0800

    added target dependencies
    Signed-off-by: George Jahad <george@georgejahad.com>

--- scripts/createImage.sh
+++ scripts/createImage.sh
@@ -2,12 +2,21 @@
 set -e
 SPARK_ROOT=$1
 IMAGE_NAME=$2
-mvn --batch-mode package
-cp target/armada-cluster-manager-1.0.0-SNAPSHOT.jar /home/gbj/incoming/spark/assembly/target/scala-2.13/jars
-cp ~/.m2/repository/com/thesamet/scalapb/lenses_2.13/0.11.17/lenses_2.13-0.11.17.jar /home/gbj/incoming/spark/assembly/target/scala-2.13/jars
-cp ~/.m2/repository/com/thesamet/scalapb/scalapb-runtime_2.13/0.11.17/scalapb-runtime_2.13-0.11.17.jar /home/gbj/incoming/spark/assembly/target/scala-2.13/jars
-cp ~/.m2/repository/com/thesamet/scalapb/scalapb-runtime-grpc_2.13/0.11.13/scalapb-runtime-grpc_2.13-0.11.13.jar /home/gbj/incoming/spark/assembly/target/scala-2.13/jars
-cp ~/.m2/repository/io/armadaproject/armada/scala-armada-client_2.13/0.1.0-SNAPSHOT/scala-armada-client_2.13-0.1.0-SNAPSHOT.jar /home/gbj/incoming/spark/assembly/target/scala-2.13/jars
+
+mvn --batch-mode package dependency:copy-dependencies
+dependencies=(
+    target/armada-cluster-manager-1.0.0-SNAPSHOT.jar
+    target/dependency/lenses_2.13-0.11.13.jar
+    target/dependency/scalapb-runtime_2.13-0.11.13.jar
+    target/dependency/scalapb-runtime-grpc_2.13-0.11.13.jar
+    target/dependency/scala-armada-client_2.13-0.1.0-SNAPSHOT.jar
+)
+
+# Copy dependencies to the docker image directory
+for d in ${dependencies[@]}; do
+    cp $d $SPARK_ROOT/assembly/target/scala-2.13/jars
+done
+
 cp scripts/entrypoint.sh $SPARK_ROOT/resource-managers/kubernetes/docker/src/main/dockerfiles/spark
 cd $SPARK_ROOT
 ./bin/docker-image-tool.sh -t $IMAGE_NAME build
