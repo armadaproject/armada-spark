0d8cacacaa1c8a855a68c83c4260e7492b356a7f
GeorgeJahad <github@blackbirdsystems.net>
2025-03-14 09:54:20 -0700

    Add spark3 support (#5)

--- scripts/submitSparkPi.sh
+++ scripts/submitSparkPi.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-#command should be run like so:
-#  examples/submitSparkPi.sh armada://localhost:30002 ~/incoming/spark
+source scripts/init.sh
 
-ARMADA_MASTER=$1
-SPARK_ROOT=$2
-ARMADA_SPARK_ROOT=`pwd`
-ARMADA_QUEUE=test
-# Ensure our "test" queue exists on Armada
+# Ensure queue exists on Armada
 if ! armadactl get queue $ARMADA_QUEUE >& /dev/null; then
   armadactl create queue $ARMADA_QUEUE
 fi
 
 # run spark pi app on armada.  Note we are using the "ArmadaSparkSubmit" class below
 #  instead of the standard "SparkSubmit".  The rest of the parameters are standard spark
-$SPARK_ROOT/bin/spark-class org.apache.spark.deploy.ArmadaSparkSubmit \
+$SPARK_HOME/bin/spark-class org.apache.spark.deploy.ArmadaSparkSubmit \
   --master $ARMADA_MASTER --deploy-mode cluster \
-  --jars $ARMADA_SPARK_ROOT/target/armada-cluster-manager-1.0.0-SNAPSHOT.jar \
   --name spark-pi \
   --class org.apache.spark.examples.SparkPi \
   --conf spark.executor.instances=2 \
-  --conf spark.kubernetes.container.image=spark:testing \
-  local:///opt/spark/examples/jars/spark-examples.jar 100
+  --conf spark.kubernetes.container.image=spark:$IMAGE_NAME \
+  local:///opt/spark/examples/jars/spark-examples_${SCALA_BIN_VERSION}-${SPARK_VERSION}.jar 100
