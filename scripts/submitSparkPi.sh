62c848547fd2f1be07522fb70a3f7c9ce40a992e
Enrico Minack <github@enrico.minack.dev>
2025-03-19 17:30:14 +0100

    Use Spark Docker image, drop compiling Spark
    Signed-off-by: Enrico Minack <github@enrico.minack.dev>

--- scripts/submitSparkPi.sh
+++ scripts/submitSparkPi.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
+set -euo pipefail
 
-source scripts/init.sh
+# init environment variables
+scripts="$(cd "$(dirname "$0")"; pwd)"
+source "$scripts/init.sh"
 
 # Ensure queue exists on Armada
 if ! armadactl get queue $ARMADA_QUEUE >& /dev/null; then
   armadactl create queue $ARMADA_QUEUE
 fi
 
-# run spark pi app on armada.  Note we are using the "ArmadaSparkSubmit" class below
-#  instead of the standard "SparkSubmit".  The rest of the parameters are standard spark
-$SPARK_HOME/bin/spark-class org.apache.spark.deploy.ArmadaSparkSubmit \
-  --master $ARMADA_MASTER --deploy-mode cluster \
-  --name spark-pi \
-  --class org.apache.spark.examples.SparkPi \
-  --conf spark.kubernetes.container.image=spark:$IMAGE_NAME \
-  local:///opt/spark/examples/jars/spark-examples_${SCALA_BIN_VERSION}-${SPARK_VERSION}.jar 100
+# needed by kind load docker-image (if docker is installed via snap)
+# https://github.com/kubernetes-sigs/kind/issues/2535
+export TMPDIR="$scripts/.tmp"
+mkdir -p "$TMPDIR"
+kind load docker-image $IMAGE_NAME --name armada
+
+# run spark pi example via Docker image
+docker run --rm --network host $IMAGE_NAME \
+    /opt/spark/bin/spark-class org.apache.spark.deploy.ArmadaSparkSubmit \
+    --master $ARMADA_MASTER --deploy-mode cluster \
+    --name spark-pi \
+    --class org.apache.spark.examples.SparkPi \
+    --conf spark.executor.instances=1 \
+    --conf spark.kubernetes.container.image=$IMAGE_NAME \
+    --conf spark.armada.lookouturl=$ARMADA_LOOKOUT_URL \
+    local:///opt/spark/examples/jars/spark-examples_${SCALA_BIN_VERSION}-${SPARK_VERSION}.jar 100
