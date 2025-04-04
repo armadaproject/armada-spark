206c50e78ea1f137244f8fd9c538a6035cb94412
Enrico Minack <github@enrico.minack.dev>
2025-04-04 18:01:53 +0200

    Rework `createImage.sh` script (#19)
    - defines default values for `IMAGE_NAME`, `ARMADA_MASTER`, `ARMADA_QUEUE`, `ARMADA_LOOKOUT_URL`
    - derives Spark and Scala version from `armada-spark/pom.xml`, set via `./scripts/set-version.sh` before script runs
    - build Docker image based on released Spark image, builds in 20s rather than 10m
    - build Docker image based on released jars if no Docker image released (no jars built from Spark sources), builds in 3m
    - Spark sources automatically checked out via git if needed
    - adds e2e CI job which performs the whole procedure needed to submit SparkPi job
    - image name `$IMAGE_NAME` is now meant to include `spark:` prefix, e.g. `spark:armada`
    - scripts can be executed from any path, not just from `armada-spark/`
    
    ---------
    
    Signed-off-by: Enrico Minack <github@enrico.minack.dev>
    Co-authored-by: GeorgeJahad <github@blackbirdsystems.net>

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,22 +2,23 @@
 
 # Utility functions
 
-if [ -e "scripts/config.sh" ]; then
-    source scripts/config.sh
+root="$(cd "$(dirname "$0")/.."; pwd)"
+scripts="$(cd "$(dirname "$0")"; pwd)"
+
+if [ -e "$scripts/config.sh" ]; then
+    source "$scripts/config.sh"
 fi
 
 print_usage () {
     echo ' Usage:'
     echo '   -h  help'
-    echo '   -s  <spark-home>'
     echo '   -i  <image-name>'
     echo '   -m  <armada-master-url>'
     echo '   -q  <armada-queue>'
     echo '   -l  <armada-lookout-url>'
     echo ''
     echo 'You also can specify those parameters in scripts/config.sh, like so:'
-    echo '   SPARK_HOME=../spark'
-    echo '   IMAGE_NAME=testing'
+    echo '   IMAGE_NAME=spark:armada'
     echo '   ARMADA_MASTER=armada://localhost:30002'
     echo '   ARMADA_QUEUE=test'
     echo '   ARMADA_LOOKOUT_URL=http://localhost:30000'
@@ -27,7 +28,6 @@ print_usage () {
 while getopts "hs:i:l:m:q" opt; do
   case "$opt" in
     h) print_usage ;;
-    s) SPARK_HOME=$OPTARG ;;
     i) IMAGE_NAME=$OPTARG ;;
     m) ARMADA_MASTER=$OPTARG ;;
     q) ARMADA_QUEUE=$OPTARG ;;
@@ -35,23 +35,23 @@ while getopts "hs:i:l:m:q" opt; do
   esac
 done
 
-get_scala_version () {
-    version=$(grep '<scala.version>' $SPARK_HOME/pom.xml | head -1  | grep -oP '(?<=<scala.version>).*?(?=</scala.version>)')
-    bin_version=$(grep '<scala.binary.version>' $SPARK_HOME/pom.xml | head -1  | grep -oP '(?<=<scala.binary.version>).*?(?=</scala.binary.version>)')
-    echo "$version $bin_version"
-}
+export IMAGE_NAME="${IMAGE_NAME:-spark:armada}"
+export ARMADA_MASTER="${ARMADA_MASTER:-armada://localhost:30002}"
+export ARMADA_QUEUE="${ARMADA_QUEUE:-test}"
+export ARMADA_LOOKOUT_URL="${ARMADA_LOOKOUT_URL:-http://localhost:30000}"
 
-get_spark_version () {
-    version=$(grep -A1 spark-parent $SPARK_HOME/pom.xml | tail -1   | grep -oP '(?<=<version>).*?(?=</version>)')
-    patch=${version/*./}
-    bin_version=${version%.${patch}}
-    echo "$version $bin_version"
-}
+# derive Scala and Spark versions from pom.xml, set via ./scripts/set-version.sh
+if [[ -z "${SCALA_VERSION:-}" ]]; then
+  export SCALA_VERSION=$(cd "$scripts/.."; mvn help:evaluate -Dexpression=scala.version -q -DforceStdout)
+  export SCALA_BIN_VERSION=$(cd "$scripts/.."; mvn help:evaluate -Dexpression=scala.binary.version -q -DforceStdout)
+fi
+if [[ -z "${SPARK_VERSION:-}" ]]; then
+  export SPARK_VERSION=$(cd "$scripts/.."; mvn help:evaluate -Dexpression=spark.version -q -DforceStdout)
+  export SPARK_BIN_VERSION=$(cd "$scripts/.."; mvn help:evaluate -Dexpression=spark.binary.version -q -DforceStdout)
+fi
 
-read SCALA_VERSION SCALA_BIN_VERSION <<< $(get_scala_version)
-read SPARK_VERSION SPARK_BIN_VERSION <<< $(get_spark_version)
-
-if ! [ -e src/main/scala-spark-$SPARK_BIN_VERSION ]; then
-    echo This tool does not support Spark version $SPARK_VERSION . Exiting
+# check the Spark version is supported
+if ! [ -e "$root/src/main/scala-spark-$SPARK_BIN_VERSION" ]; then
+    echo "This tool does not support Spark version ${SPARK_VERSION}."
     exit 1
 fi
