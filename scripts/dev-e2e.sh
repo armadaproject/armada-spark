810185372d36d0169aaf58428754056b9ecd44f9
Rich Scott <richscott@sent.com>
2025-05-06 21:59:22 -0600

    A script to do local basic smoke-testing of armada-spark.
    This script submits a spark-armada job (after optionally starting Armada
    locally via the armada-operator repository), and queries Armada in
    various ways to check if the submitted job(s) have run.  It assumes the
    spark-armada image has been built and is in the local Docker repository.
    
    Signed-off-by: Rich Scott <richscott@sent.com>

---
+++ scripts/dev-e2e.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+STATUSFILE='armadactl-query.txt'
+AOREPO='https://github.com/armadaproject/armada-operator.git'
+AOHOME=../armada-operator
+ARMADA_CTL_VERSION='0.18.3'
+TEST_QUEUE='test'
+JOB_DETAILS=1
+
+usage() {
+  cat > /dev/stderr  <<USAGE
+Usage: ${0} -s
+  -s => get job status details from Armada
+
+USAGE
+  exit 1
+}
+
+while getopts "s" opt; do
+    case "${opt}" in
+        s )
+          JOB_DETAILS=1 ;;
+        * )
+          echo "opt is $opt"
+          usage ;;
+    esac
+done
+
+scripts="$(cd "$(dirname "$0")" || exit; pwd)"
+source "$scripts"/init.sh
+
+log() {
+    echo -e "${GREEN}$1${NC}"
+}
+
+
+fetch-armadactl() {
+  os=$(uname -s)
+  arch=$(uname -m)
+  dl_url='https://github.com/armadaproject/armada/releases/download'
+
+  if [ "$os" = "Darwin" ]; then
+    dl_os='darwin'
+  elif [ "$os" = "Linux" ]; then
+    dl_os='linux'
+ else
+    log "fetch-armadactl(): sorry, operating system $os not supported; exiting now"
+    exit 1
+  fi
+
+  if [ "$arch" = 'arm64' ]; then
+    dl_arch='arm64'
+  elif [ "$arch" = 'x86_64' ]; then
+    dl_arch='amd64'
+  else
+    log "fetch-armadactl(): sorry, architecture $arch not supported; exiting now"
+    exit 1
+  fi
+
+  dl_file=armadactl_${ARMADA_CTL_VERSION}_${dl_os}_${dl_arch}.tar.gz
+
+  curl -s -LO ${dl_url}/v${ARMADA_CTL_VERSION}/$dl_file
+  tar xzf $dl_file armadactl
+}
+
+armadactl-retry() {
+  for _ in {1..10}; do
+    if ./armadactl "$@" > /dev/null 2>&1; then
+      return 0
+    fi
+    sleep 5
+  done
+  log "armadactl command failed after 10 attempts" >&2
+  exit 1
+}
+
+start-armada() {
+  if [ -d $AOHOME ]; then
+    log "Using existing armada-operator repo at $AOHOME"
+  else
+    if ! git clone $AOREPO $AOHOME; then
+      log "There was a problem cloning the armada-operator repo; exiting now"
+      exit 1
+    fi
+
+
+    log "Patching armada-operator"
+    if ! patch -p1 -d $AOHOME/ < ./e2e/armada-operator.patch; then
+      log "There was an error patching the repo copy $AOHOME"
+      exit 1
+    fi
+  fi
+
+  cd $AOHOME || exit 1
+  log  "Running 'make kind-all' to install and start Armada; this may take up to 6 minutes"
+  if ! make kind-all 2>&1 | tee armada-start.txt; then
+    echo ""
+    log "There was a problem starting Armada"
+    log "Exiting now"
+    exit 1
+  fi
+  cd ..
+}
+
+main() {
+  log "Checking for armadactl .."
+  if ! test -x armadactl; then
+    log "Fetching armadactl..."
+    fetch-armadactl
+  fi
+
+  log "Checking to see if Armada cluster is available ..."
+
+  if ! armadactl get queues > $STATUSFILE 2>&1 ; then
+    if grep -q 'connection refused' $STATUSFILE; then
+      log "Using armada-operator to start Armada cluster; this may take up to 5 minutes"
+      start-armada
+      sleep 5
+      armadactl-retry get queues
+    else
+      log "FAILED: output is "
+      cat $STATUSFILE
+      echo ""
+      log "Armada cluster appears to be running, but an unknown error has happened"
+      log "Exiting now"
+      exit 1
+    fi
+  else
+    log "Armada is available"
+    rm armadactl-query.txt
+  fi
+
+  log "Creating $TEST_QUEUE queue..."
+  armadactl-retry create queue "$TEST_QUEUE"
+
+  log "Loading Docker image spark:$IMAGE_NAME into Armada cluster"
+  $AOHOME/bin/tooling/kind load docker-image "$IMAGE_NAME" --name armada
+
+  log "Submitting SparkPI job"
+  scripts/submitSparkPi.sh  2>&1 | tee submitSparkPi.log
+
+  DRIVER_JOBID=$(grep '^Driver JobID:' submitSparkPi.log | awk '{print $3}')
+  EXECUTOR_JOBIDS=$(grep '^Executor JobID:' submitSparkPi.log | awk '{print $3}')
+
+  if [ $JOB_DETAILS = 1 ]; then
+    sleep 5   # wait a moment for Armada to schedule & run the job
+
+    timeout 1m armadactl watch test driver --exit-if-inactive 2>&1 | tee armadactl.watch.log
+    if grep "Job failed:" armadactl.watch.log; then echo "Job failed"; exit 1; fi
+
+    log "Driver Job Spec  $DRIVER_JOBID"
+    curl --silent --show-error -X POST "http://localhost:30000/api/v1/jobSpec" \
+      --json "{\"jobId\":\"$$DRIVER_JOBID\"}" | jq
+    for exec_jobid in $EXECUTOR_JOBIDS ; do
+      log "Executor Job Spec  $exec_jobid"
+      curl --silent --show-error -X POST "http://localhost:30000/api/v1/jobSpec" \
+          --json "{\"jobId\":\"$exec_jobid\"}" | jq
+    done
+  fi
+}
+
+main
