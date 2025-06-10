e492183858b6c6c079b342f028295ffaa009f6d4
George Jahad <github@blackbirdsystems.net>
2025-06-10 13:40:25 -0700

    extraFiles/config/specify scripts
    Signed-off-by: George Jahad <github@blackbirdsystems.net>

--- scripts/submitSparkPi.sh
+++ scripts/submitSparkPi.sh
@@ -10,17 +10,17 @@ source "$scripts/init.sh"
 JOBSET="${JOBSET:-armada-spark}"
 
 if [ "${INCLUDE_PYTHON}" == "false" ]; then
-    echo Running Scala Spark
     NAME=spark-pi
     CLASS_PROMPT="--class"
-    CLASS_ARG=org.apache.spark.examples.SparkPi
-    FIRST_ARG="local:///opt/spark/examples/jars/spark-examples_${SCALA_BIN_VERSION}-${SPARK_VERSION}.jar"
+    CLASS_ARG="$SCALA_CLASS"
+    FIRST_ARG="$CLASS_PATH"
+    echo Running Scala Spark: $CLASS_ARG $FIRST_ARG "${FINAL_ARGS[@]}"
 else
-    echo Running Python Spark
     NAME=python-pi
     CLASS_PROMPT=""
     CLASS_ARG=""
-    FIRST_ARG=/opt/spark/examples/src/main/python/pi.py
+    FIRST_ARG="$PYTHON_SCRIPT"
+    echo Running Python Spark: $FIRST_ARG "${FINAL_ARGS[@]}"
 fi
 
 if [ "${USE_KIND}" == "true" ]; then
@@ -42,30 +42,12 @@ else
     AUTH_ARG=""
 fi
 
-
-# run spark pi example via Docker image
-docker run --rm --network host $IMAGE_NAME \
+docker run -v $scripts/../conf:/opt/spark/conf --rm --network host $IMAGE_NAME \
     /opt/spark/bin/spark-class org.apache.spark.deploy.ArmadaSparkSubmit \
     --master $ARMADA_MASTER --deploy-mode cluster \
     --name $NAME \
     $CLASS_PROMPT $CLASS_ARG \
     $AUTH_ARG \
-    --conf spark.armada.internalUrl=armada-server.armada:50051 \
-    --conf spark.armada.queue=$ARMADA_QUEUE \
-    --conf spark.armada.jobSetId=armada-spark \
-    --conf spark.executor.instances=4 \
-    --conf spark.jars.ivy=/tmp/.ivy \
+    --conf spark.home=/opt/spark \
     --conf spark.armada.container.image=$IMAGE_NAME \
-    --conf spark.armada.lookouturl=$ARMADA_LOOKOUT_URL \
-    --conf spark.armada.scheduling.nodeUniformity=armada-spark \
-    --conf spark.armada.scheduling.nodeSelectors=armada-spark=true \
-    --conf spark.armada.pod.labels=foo=bar \
-    --conf spark.armada.driver.limit.cores=200m \
-    --conf spark.armada.driver.limit.memory=450Mi \
-    --conf spark.armada.driver.request.cores=200m \
-    --conf spark.armada.driver.request.memory=450Mi \
-    --conf spark.armada.executor.limit.cores=100m \
-    --conf spark.armada.executor.limit.memory=510Mi \
-    --conf spark.armada.executor.request.cores=100m \
-    --conf spark.armada.executor.request.memory=510Mi \
-    $FIRST_ARG 100
+    $FIRST_ARG  "${FINAL_ARGS[@]}"
