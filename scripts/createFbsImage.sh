30ad20ba976d73d822447eb84a86e97704232471
George Jahad <github@blackbirdsystems.net>
2026-01-09 17:52:19 -0800

    added createFbsImage.sh

---
+++ scripts/createFbsImage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+repo=${FALLBACK_STORAGE_REPO:-https://github.com/G-Research/spark}
+repo_dir=`basename $repo`
+branch=${FALLBACK_STORAGE_BRANCH:-fallback-storage-v3.5.3}
+# create the spark image with fallback storage support
+git clone $repo
+cd $dir
+git checkout $branch
+#override devpod setting
+export SPARK_HOME=`pwd`
+./build/mvn clean install --batch-mode -Dscalastyle.skip=true -DskipTests  -Pkubernetes -Pscala-2.12
+./bin/docker-image-tool.sh -u 185 -t "spark.fbs.img2"  -p ./resource-managers/kubernetes/docker/src/main/dockerfiles/spark/bindings/python/Dockerfile build
