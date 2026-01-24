131b350a0a1bc3d84b6491f7773dbf8673a88651
George Jahad <github@blackbirdsystems.net>
2026-01-23 16:42:42 -0800

    cleanup

--- scripts/createFbsImage.sh
+++ scripts/createFbsImage.sh
@@ -18,9 +18,9 @@ tmp_dir=$(mktemp -d)
 echo Creating temp dir: $tmp_dir
 cd $tmp_dir
 
-repo=${ARMADA_FALLBACK_STORAGE_REPO:-https://github.com/G-Research/spark}
+repo=${ARMADA_FALLBACK_STORAGE_REPO:-https://github.com/GeorgeJahad/spark}
 repo_dir=`basename $repo`
-branch=${ARMADA_FALLBACK_STORAGE_BRANCH:-fallback-storage-v3.5.3}
+branch=${ARMADA_FALLBACK_STORAGE_BRANCH:-remote-storage-backed-shuffle-v3.5.3}
 
 # create the spark image with fallback storage support
 git clone $repo
